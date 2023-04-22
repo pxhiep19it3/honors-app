65e6bd09951bf1cdbed44d643b4cc5de93c59967
DESKTOP-TOT4CLQ\Mr Bingi <hiepphan197420@gmail.com>
2023-04-23 00:39:18 +0700

    Đổi tên, thay icon

--- lib/common/values/app.images.dart
+++ lib/common/values/app.images.dart
@@ -2,6 +2,4 @@ class AppImage {
   static const String path = 'assets/image';
   static const String logo = '$path/logo.png';
   static const String google = '$path/gg.png';
-  static const String testLogo = '$path/testLogo.png';
-  static const String vku = '$path/vku.png';
 }
